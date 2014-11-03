9cd79de5b4f0a7507943bc5d8484f78ad88bde04
Alex Cannon <acannon828@gmail.com>
2014-11-03 00:56:13 -0500

    authentication working, need to fix sp.search
    need to query for both artist and track name, but all spotipy sp.search
    examples only use one field for the query, and using
    'track:<trackname>+artist:<artistname' doesn't work.
    
    https://developer.spotify.com/web-api/search-item/

--- myMusicImporter.py
+++ myMusicImporter.py
@@ -1,30 +1,27 @@
+import os
+import sys
 import spotipy
 import spotipy.util as util
-from rdio.rdio import Rdio
+from rdioapi import Rdio
+from urllib2 import HTTPError
 
 def rdioCollectionForUser(rdio, user):
 	"""
 	
 	"""
 	try:
-    	# authenticate against the Rdio service
-    	url = rdio.begin_authentication('oob')
-    	print("Go to: %s" % url)
-    	verifier = raw_input("Then enter the code: ").strip()
-    	rdio.complete_authentication(verifier)
+		# authenticate against the Rdio service
+		url = rdio.begin_authentication('oob')
+		print 'Go to: %s' % (url)
+		verifier = raw_input("Then enter the code: ").strip()
+		rdio.complete_authentication(verifier)
 
-    	# get tracks in collection
-    	tracks = rdio.call('getTracksInCollection',
-                       {'sort': 'artist', 'extras': '-*,key'})
-    	if (tracks['status'] == 'ok'):
-    		return tracks
-		else:
-        	print("ERROR: %s" % tracks['message'])
-			return 1 #TODO: Return an error message?
+		# get tracks in collection
+		tracks = rdio.call('getTracksInCollection')
 
 	except HTTPError as e:
-    	# if we have a protocol error, print it
-    	print(e.read())	
+		# if we have a protocol error, print it
+		print(e.read())	
 		return 1 #TODO: Return an error message?
 
 	return tracks 
@@ -34,9 +31,9 @@ def spotifyTrackIDForSong(sp, songName, artist):
 	Returns the Spotify track ID of the first song found matching the given
 	song name and artist. 
 	"""
-	query = "artist:" + artist + "+track:" + songName + "&type=track"
+	query = "artist:" + artist + "+track:" + songName
 	#TODO: What if search results are empty?
-	return sp.search(query, limit=1)[0]['trackID']
+	return sp.search(query, type='track',limit=1)[0]['trackID']
 
 def main(argv):
 	if len(argv) != 3:
@@ -47,13 +44,13 @@ def main(argv):
 	rdioUser = argv[2]
 
 	scope = 'user-library-modify'
-	token = util.prompt_for_user_token(username, scope)
+	token = util.prompt_for_user_token(spotifyUser, scope)
 	
 	RDIO_CONSUMER_KEY = os.getenv('RDIO_CONSUMER_KEY')
 	RDIO_CONSUMER_SECRET = os.getenv('RDIO_CONSUMER_SECRET')
 
 	if RDIO_CONSUMER_KEY and RDIO_CONSUMER_SECRET:
-		rdio = Rdio((RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET))
+		rdio = Rdio(RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET, {})
 		rdioCollection = rdioCollectionForUser(rdio, rdioUser)
 	else:
 		print "Both RDIO_CONSUMER_KEY and RDIO_CONSUMER_SECRET must be \
@@ -61,12 +58,16 @@ def main(argv):
 		return 1 #TODO: return an error code?
 
 	tids = [] # spotify track ids
-	for rdioTrack in rdioCollection:
-		tids.append(spotifyTrackIDForSong(sp, rdioTrack['name'], rdioTrack['artist']))
 	if token:
 		sp = spotipy.Spotify(auth=token)
+		for rdioTrack in rdioCollection:
+			tids.append(spotifyTrackIDForSong(sp, rdioTrack['name'], rdioTrack['artist']))
 		results = sp.current_user_saved_tracks_add(tracks=tids)
 		#TODO: What to do with results?	
 		sp.trace = False
 	else:
-		print "Can't get token for ", spotifyUser	
+		print "Can't get token for ", spotifyUser
+
+
+if __name__ == '__main__':
+	main(sys.argv)
